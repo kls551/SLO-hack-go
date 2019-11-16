80ef092e1856b4bd9649473a38a6882a224215ed
Lien (Ally) Quan <liquan@calpoly.edu>
2019-11-16 12:58:58 -0800

    added parking map

--- slohack/src/main.js
+++ slohack/src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import  b-container from 'bootstrap-vue';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+// import 'my_style.css';
 
 Vue.config.productionTip = false
-
+require('./assets/my_style.css');
 Vue.use(BootstrapVue)
 new Vue({
   render: h => h(App),
