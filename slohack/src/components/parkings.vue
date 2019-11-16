3ba5c823062437529842bd5ad655f7dfdb9bb471
Kyaw Soe <32246763+kls551@users.noreply.github.com>
2019-11-16 14:06:06 -0800

    Update parkings.vue

--- slohack/src/components/parkings.vue
+++ slohack/src/components/parkings.vue
@@ -6,33 +6,25 @@
           v-for="lvl in levels"
           :key="lvl.level"
           :title="JSON.stringify(lvl.level)" active @click="getParkings(lvl.level)">
-          
-          <b-container class='my-5'>
+          <b-container>
             <b-row class="parking-row">
-              <b-col class="available">1 of 2</b-col>
-              <b-col class="not-available">2 of 2</b-col>
-              <b-col class="available">1 of 2</b-col>
-              <b-col class="not-available">2 of 2</b-col>
-              <b-col class="available">1 of 2</b-col>
-              <b-col class="not-available">2 of 2</b-col>
-              <b-col class="available">1 of 2</b-col>
-              <b-col class="not-available">2 of 2</b-col>
+              <b-col
+                v-for="value in parkings.slice(0, 5)"
+                :key="value.id"
+                :class="value.taken ? 'not-available' : 'available'"
+              >
+                {{ value.id }}</b-col>
             </b-row>
 
             <b-row class="parking-row">
-              <b-col class="not-available">2 of 2</b-col>
-              <b-col class="available">1 of 2</b-col>
-              <b-col class="not-available">2 of 2</b-col>
-              <b-col class="available">1 of 2</b-col>
-              <b-col class="not-available">2 of 2</b-col>
-              <b-col class="available">1 of 2</b-col>
-              <b-col class="not-available">2 of 2</b-col>
-              <b-col class="available">1 of 2</b-col>
+              <b-col
+                v-for="value in parkings.slice(5)"
+                :key="value.id"
+                :class="value.taken ? 'not-available' : 'available'"
+              >
+                {{ value.id }}</b-col>
             </b-row>
           </b-container>
-        </b-tab>
-        <b-tab title="Level 2" @click="getParkings(2)">
-          <b-card-text>Tab contents 2</b-card-text>
         </b-tab>
       </b-tabs>
     </b-card>
